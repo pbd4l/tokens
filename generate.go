fb978a69bd20fbed3f6f179aa550baa182a48e25
Peter Byfield <byfield554+ghd4l@gmail.com>
2021-06-11 19:27:12 +0200

    Use cobra for nicer CLI

--- generate.go
+++ generate.go
@@ -2,65 +2,68 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"fmt"
 	"io"
 	"math/rand"
 	"os"
 	"strings"
 	"time"
+
+	"github.com/spf13/cobra"
 )
 
-// Generate generates random tokens and writes them to a file
-func Generate(args []string) error {
-	flagSet := flag.NewFlagSet("generate", flag.ContinueOnError)
-
-	file := flagSet.String("file", "tokens.txt", "file to write the tokens.")
-	n := flagSet.Int("n", 1e7, "number of tokens to generate.")
-	seed := flagSet.Int64("seed", -1, "seed for random token generation. passing a negative value will use the current time as a seed.")
-
-	if err := flagSet.Parse(args); err != nil {
-		return fmt.Errorf("could not parse flags: %w", err)
+func generateCmd() *cobra.Command {
+	var file string
+	var number int
+	var seed int64
+	cmd := &cobra.Command{
+		Use:   "generate",
+		Short: "Generate random tokens",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var w io.Writer = os.Stdout
+			if file != "" {
+				f, err := os.Create(file)
+				if err != nil {
+					return fmt.Errorf("could not create tokens file: %w", err)
+				}
+				defer f.Close()
+				w = f
+			}
+			return generate(w, number, seed)
+		},
 	}
-
-	f, err := os.Create(*file)
-	if err != nil {
-		return fmt.Errorf("could not create tokens file: %w", err)
-	}
-	defer f.Close()
-
-	return generate(f, *n, *seed)
+	cmd.Flags().StringVarP(&file, "file", "f", "", "file to save the generated tokens. will write to stdout if omitted")
+	cmd.Flags().IntVarP(&number, "number", "n", 1e7, "number of tokens to generate")
+	cmd.Flags().Int64VarP(&seed, "seed", "s", -1, "seed for random token generation. passing a negative value will use the current time as a seed")
+	return cmd
 }
 
-func generate(w io.Writer, n int, seed int64) error {
+func generate(w io.Writer, number int, seed int64) error {
 	if seed < 0 {
 		seed = time.Now().Unix()
 	}
 	rand.Seed(seed)
 
 	bw := bufio.NewWriter(w)
-	for i := 0; i < n; i++ {
-		token, err := randomToken()
+	for i := 0; i < number; i++ {
+		_, err := bw.WriteString(randomToken() + "\n")
 		if err != nil {
-			return fmt.Errorf("could not generate token: %w", err)
-		}
-		if _, err := bw.WriteString(token + "\n"); err != nil {
 			return fmt.Errorf("could not write token: %w", err)
 		}
 	}
-	if err := bw.Flush(); err != nil {
+	err := bw.Flush()
+	if err != nil {
 		return fmt.Errorf("could not flush writer: %w", err)
 	}
 
 	return nil
 }
 
-func randomToken() (string, error) {
+func randomToken() string {
 	var sb strings.Builder
 	for i := 0; i < 7; i++ {
-		if _, err := sb.WriteRune('a' + rune(rand.Intn(26))); err != nil {
-			return "", fmt.Errorf("could not write token rune: %w", err)
-		}
+		sb.WriteRune('a' + rune(rand.Intn(26)))
 	}
-	return sb.String(), nil
+	return sb.String()
 }
--- import.go
+++ import.go
@@ -3,108 +3,136 @@ package main
 import (
 	"bufio"
 	"database/sql"
-	"flag"
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"regexp"
 	"strings"
 
 	_ "github.com/lib/pq"
+	"github.com/spf13/cobra"
 )
 
-// Import imports tokens from a file into a postgres database
-func Import(args []string) error {
-	flagSet := flag.NewFlagSet("import", flag.ContinueOnError)
-
-	dsn := flagSet.String("dsn", "", "postgres data-source-name.")
-	file := flagSet.String("file", "tokens.txt", "file to import the tokens from.")
-
-	if err := flagSet.Parse(args); err != nil {
-		return fmt.Errorf("could not parse flags: %w", err)
+func importCmd() *cobra.Command {
+	var dsn string
+	var file string
+	var bs int
+	cmd := &cobra.Command{
+		Use:   "import",
+		Short: "Import tokens to a postgres database",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var r io.Reader = os.Stdin
+			if file != "" {
+				f, err := os.Open(file)
+				if err != nil {
+					return fmt.Errorf("could not open tokens file: %w", err)
+				}
+				defer f.Close()
+				r = f
+			}
+			return importTokens(r, dsn, bs, cmd.ErrOrStderr())
+		},
 	}
-	if *dsn == "" {
-		return fmt.Errorf("flag -dsn is required")
-	}
+	cmd.Flags().StringVarP(&dsn, "dsn", "", "", "postgres data-source-name")
+	must(cmd.MarkFlagRequired("dsn"))
+	cmd.Flags().StringVarP(&file, "file", "f", "", "file to read the tokens from. will read from stdin if omitted")
+	cmd.Flags().IntVarP(&bs, "bs", "b", 10000, "batch size used for bulk import")
+	return cmd
+}
 
-	db, err := sql.Open("postgres", *dsn)
+var tokenRe = regexp.MustCompile(`^[a-z]{7}$`)
+
+func importTokens(r io.Reader, dsn string, bs int, outErr io.Writer) error {
+	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return fmt.Errorf("could not open database: %w", err)
 	}
-	if err = db.Ping(); err != nil {
+	err = db.Ping()
+	if err != nil {
 		return fmt.Errorf("could not ping database: %w", err)
 	}
 	defer db.Close()
 
-	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS tokens (token CHAR(7) PRIMARY KEY)"); err != nil {
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS tokens (token CHAR(7) PRIMARY KEY)")
+	if err != nil {
 		return fmt.Errorf("could not create/ensure table: %w", err)
 	}
 
-	tokensFile, err := os.Open(*file)
-	if err != nil {
-		return fmt.Errorf("could not open tokens file: %w", err)
+	q := newImportTokensQuery(bs)
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		token := scanner.Text()
+		if !tokenRe.MatchString(token) {
+			fmt.Fprintf(outErr, "token %s is invalid, skipping\n", token)
+			continue
+		}
+		err = q.AddToken(token)
+		if err != nil {
+			return fmt.Errorf("could not add token to query: %w", err)
+		}
+		if q.Full() {
+			err = q.Exec(db)
+			if err != nil {
+				return fmt.Errorf("could not execute query: %w", err)
+			}
+		}
 	}
-	defer tokensFile.Close()
-
-	err = importTokens(db, tokensFile)
+	err = q.Exec(db)
 	if err != nil {
-		return fmt.Errorf("could not import tokens: %w", err)
+		return fmt.Errorf("could not execute query: %w", err)
+	}
+	err = scanner.Err()
+	if err != nil {
+		return fmt.Errorf("could not scan tokens: %w", err)
 	}
 
 	return nil
 }
 
-var tokenRe = regexp.MustCompile(`^[a-z]{7}$`)
+type importTokensQuery struct {
+	sb   strings.Builder
+	args []interface{}
+	bs   int
+}
 
-func importTokens(db *sql.DB, r io.Reader) error {
-	bs := 10000
+func newImportTokensQuery(bs int) *importTokensQuery {
+	var q importTokensQuery
+	q.sb.WriteString("INSERT INTO tokens (token) VALUES ")
+	q.args = make([]interface{}, 0, q.bs)
+	q.bs = bs
+	return &q
+}
 
-	scanner := bufio.NewScanner(r)
-
-	var sb strings.Builder
-	sb.WriteString("INSERT INTO tokens (token) VALUES ")
-	i := 0
-	args := make([]interface{}, 0, bs)
-	for scanner.Scan() {
-		token := scanner.Text()
-		if !tokenRe.MatchString(token) {
-			log.Printf("token %s is invalid, skipping\n", token)
-			continue
-		}
-
-		if i == 0 {
-			sb.WriteString(fmt.Sprintf("($%d)", i+1))
-		} else {
-			sb.WriteString(fmt.Sprintf(",($%d)", i+1))
-		}
-		i++
-		args = append(args, token)
-
-		if i == bs {
-			sb.WriteString(" ON CONFLICT DO NOTHING")
-			_, err := db.Exec(sb.String(), args...)
-			if err != nil {
-				return fmt.Errorf("could not insert tokens: %w", err)
-			}
-			sb.Reset()
-			sb.WriteString("INSERT INTO tokens (token) VALUES ")
-			i = 0
-			args = make([]interface{}, 0, bs)
-		}
+func (q *importTokensQuery) AddToken(token string) error {
+	l := len(q.args)
+	if l == q.bs {
+		return fmt.Errorf("query is full")
 	}
-
-	if i > 0 {
-		sb.WriteString(" ON CONFLICT DO NOTHING")
-		_, err := db.Exec(sb.String(), args[:i]...)
-		if err != nil {
-			return fmt.Errorf("could not insert tokens: %w", err)
-		}
+	if l == 0 {
+		q.sb.WriteString(fmt.Sprintf("($%d)", l+1))
+	} else {
+		q.sb.WriteString(fmt.Sprintf(",($%d)", l+1))
 	}
+	q.args = append(q.args, token)
+	return nil
+}
 
-	if err := scanner.Err(); err != nil {
-		return fmt.Errorf("could not scan tokens: %w", err)
+func (q *importTokensQuery) Full() bool {
+	return len(q.args) == q.bs
+}
+
+func (q *importTokensQuery) Exec(db *sql.DB) error {
+	if len(q.args) == 0 {
+		return nil
 	}
-
+	q.sb.WriteString(" ON CONFLICT DO NOTHING")
+	_, err := db.Exec(q.sb.String(), q.args...)
+	if err != nil {
+		return err
+	}
+	q.sb.Reset()
+	q.args = make([]interface{}, 0, q.bs)
+	q.sb.WriteString("INSERT INTO tokens (token) VALUES ")
 	return nil
 }
